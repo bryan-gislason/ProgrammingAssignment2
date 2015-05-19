585aa1fa7af91af1b03109f782e4bb7964584975
Bryan Gislason <bryang@krossover.com>
2015-05-18 21:16:23 -0400

    Add function methods to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## To get the inverse matrix while saving computation power,
+## construct a special matrix with makeCacheMatrix,
+## (e.g. cm <- makeCacheMatrix(matrix(rnorm(1:25, 5, 5))) ),
+## pass it to cacheSolve,
+## cacheSolve(cm), which will return the cached version
+## or recalculate the inverse if the matrix has changed
 
-## Write a short comment describing this function
-
+## makeCacheMatrix handles setting and getting a
+## matrix and its inverse using environment scopes
 makeCacheMatrix <- function(x = matrix()) {
 
+    matrixInverse <- NULL
+    set <- function(y) {
+        x <<- y
+        matrixInverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) matrixInverse <<- inverse
+    getinverse <- function() matrixInverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Gets the cached inverse of a square matrix
+cacheSolve <- function(x) {
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setinverse(m)
+    m
 }
